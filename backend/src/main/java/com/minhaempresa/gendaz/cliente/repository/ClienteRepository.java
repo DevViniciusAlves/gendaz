261f149efaed3c02f35752a339a85c6a76c38f45
Gendaz Dev <dev@gendaz.site>
2026-08-19 19:30:02 -0400

    Subindo excluir clietne corrreto

--- backend/src/main/java/com/minhaempresa/gendaz/cliente/repository/ClienteRepository.java
+++ backend/src/main/java/com/minhaempresa/gendaz/cliente/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.minhaempresa.gendaz.cliente.repository;
 
 import com.minhaempresa.gendaz.cliente.entity.ClienteEntity;
+import com.minhaempresa.gendaz.shared.enums.StatusCadastro;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
